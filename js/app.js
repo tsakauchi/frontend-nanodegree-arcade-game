b81f93ce240085ee5316bbbc386f55e0cccb2e27
Tsuginosuke Sakauchi <tsuginosuke.sakauchi@fairlandcircle.com>
2016-02-02 08:34:20 -0600

    clean: Rename constants to use upper-case per style guideline

--- js/app.js
+++ js/app.js
@@ -6,56 +6,59 @@
  /**********************************************************
  * Constants
  **********************************************************/
-var colWidth = 101;
-var rowHeight = 83;
-var numRows = 6;
-var numCols = 5;
+var COL_WIDTH = 101;
+var ROW_HEIGHT = 83;
+var NUM_OF_ROWS = 6;
+var NUM_OF_COLS = 5;
 
-var numOfEnemies = 5;
-var numOfPlayerCharacters = 5;
-var numOfObstacles = 3;
-var numOfItems = 2;
+var NUM_OF_ENEMIES = 5;
+var NUM_OF_PLAYABLE_CHARACTERS = 5;
+var NUM_OF_OBSTACLES = 3;
+var NUM_OF_ITEMS = 2;
 
-var minX = 0;
-var minXEnemy = minX - colWidth;
-var minXPlayer = minX;
-var minXSelector = minXPlayer;
-var minXObstacle = minX;
-var minXItem = minX;
+var MIN_X = 0;
+var MIN_X_ENEMY = MIN_X - COL_WIDTH;
+var MIN_X_PLAYER = MIN_X;
+var MIN_X_SELECTOR = MIN_X_PLAYER;
+var MIN_X_OBSTACLE = MIN_X;
+var MIN_X_ITEM = MIN_X;
 
-var minY = 0;
-var minYEnemy = minY + rowHeight;
-var minYPlayer = minY;
-var minYSelector = maxY - rowHeight*3;
-var minYObstacle = minY + rowHeight*4;
-var minYItem = minY + rowHeight;
+var MIN_Y = 0;
+var MIN_Y_ENEMY = MIN_Y + ROW_HEIGHT;
+var MIN_Y_PLAYER = MIN_Y;
+var MIN_Y_SELECTOR = MAX_Y - ROW_HEIGHT*3;
+var MIN_Y_OBSTACLE = MIN_Y + ROW_HEIGHT*4;
+var MIN_Y_ITEM = MIN_Y + ROW_HEIGHT;
 
-var maxX = colWidth*numCols;
-var maxXEnemy = maxX;
-var maxXPlayer = maxX - colWidth;
-var maxXSelector = maxXPlayer;
-var maxXObstacle = maxX;
-var maxXItem = maxX;
+var MAX_X = COL_WIDTH*NUM_OF_COLS;
+var MAX_X_ENEMY = MAX_X;
+var MAX_X_PLAYER = MAX_X - COL_WIDTH;
+var MAX_X_SELECTOR = MAX_X_PLAYER;
+var MAX_X_OBSTACLE = MAX_X;
+var MAX_X_ITEM = MAX_X;
 
-var maxY = rowHeight*numRows;
-var maxYEnemy = rowHeight*4;
-var maxYPlayer = maxY - rowHeight;
-var maxYSelector = maxY - rowHeight*3;
-var maxYObstacle = maxY - rowHeight*2;
-var maxYItem = rowHeight*3;
+var MAX_Y = ROW_HEIGHT*NUM_OF_ROWS;
+var MAX_Y_ENEMY = ROW_HEIGHT*4;
+var MAX_Y_PLAYER = MAX_Y - ROW_HEIGHT;
+var MAX_Y_SELECTOR = MAX_Y - ROW_HEIGHT*3;
+var MAX_Y_OBSTACLE = MAX_Y - ROW_HEIGHT*2;
+var MAX_Y_ITEM = ROW_HEIGHT*3;
 
-var iniXPlayer = Math.floor(numCols / 2) * colWidth;
-var iniYPlayer = maxYPlayer;
+var INI_X_PLAYER = Math.floor(NUM_OF_COLS / 2) * COL_WIDTH;
+var INI_Y_PLAYER = MAX_Y_PLAYER;
 
-var iniXSelector = iniXPlayer;
-var iniYSelector = maxY - rowHeight*3;
+var INI_X_SELECTOR = INI_X_PLAYER;
+var INI_Y_SELECTOR = MAX_Y - ROW_HEIGHT*3;
 
-var playerSprites = [];
-playerSprites[0] = 'images/char-boy.png';
-playerSprites[1] = 'images/char-cat-girl.png';
-playerSprites[2] = 'images/char-horn-girl.png';
-playerSprites[3] = 'images/char-pink-girl.png';
-playerSprites[4] = 'images/char-princess-girl.png';
+var PLAYER_SPRITES = [];
+PLAYER_SPRITES[0] = 'images/char-boy.png';
+PLAYER_SPRITES[1] = 'images/char-cat-girl.png';
+PLAYER_SPRITES[2] = 'images/char-horn-girl.png';
+PLAYER_SPRITES[3] = 'images/char-pink-girl.png';
+PLAYER_SPRITES[4] = 'images/char-princess-girl.png';
+
+var GAME_MODE_MAIN_GAME = 'main-game';
+var GAME_MODE_PLAYER_SELECTION = 'player-selection';
 
 /**********************************************************
  * Enemy Class
@@ -82,16 +85,16 @@ Enemy.prototype.update = function(dt) {
 
     // calculate current position
     var dx = this.speed * dt;
-    if (this.x + dx > maxXEnemy) {
-        this.x = minXEnemy;
-    } else if (this.x + dx < minXEnemy) {
-        this.x = maxXEnemy;
+    if ((this.x + dx) > MAX_X_ENEMY) {
+        this.x = MIN_X_ENEMY;
+    } else if ((this.x + dx) < MIN_X_ENEMY) {
+        this.x = MAX_X_ENEMY;
     } else {
         this.x += dx;
     }
 
     // collision detection
-    if (this.y !== player.y) return;
+    if (this.y != player.y) return;
 
     var spriteEnemy = Resources.get(this.sprite);
     var spritePlayer = Resources.get(player.sprite);
@@ -142,7 +145,7 @@ Player.prototype.update = function() {
 
     // collision detection
     // (did I 'collide' with the goal?)
-    if (this.y != minYPlayer) return;
+    if (this.y != MIN_Y_PLAYER) return;
 
     // game score manipulation
     gameScore += 200;
@@ -165,22 +168,22 @@ Player.prototype.render = function() {
 * @param {string} keyPressed, string that describes key pressed
 */
 Player.prototype.handleInput = function(keyPressed) {
-    var dx = colWidth;
-    var dy = rowHeight;
+    var dx = COL_WIDTH;
+    var dy = ROW_HEIGHT;
     if (keyPressed == 'left') {
-        if (this.x - dx >= minXPlayer) {
+        if (this.x - dx >= MIN_X_PLAYER) {
             this.x -= dx;
         }
     } else if (keyPressed == 'right') {
-        if (this.x + dx <= maxXPlayer) {
+        if (this.x + dx <= MAX_X_PLAYER) {
             this.x += dx;
         }
     } else if (keyPressed == 'up') {
-        if (this.y - dy >= minYPlayer) {
+        if (this.y - dy >= MIN_Y_PLAYER) {
             this.y -= dy;
         }
     } else if (keyPressed == 'down') {
-        if (this.y + dy <= maxYPlayer) {
+        if (this.y + dy <= MAX_Y_PLAYER) {
             this.y += dy;
         }
     }
@@ -190,8 +193,8 @@ Player.prototype.handleInput = function(keyPressed) {
 * @description Reset player state.
 */
 Player.prototype.reset = function() {
-    this.x = iniXPlayer;
-    this.y = iniYPlayer;
+    this.x = INI_X_PLAYER;
+    this.y = INI_Y_PLAYER;
 };
 
 /**********************************************************
@@ -205,8 +208,8 @@ Player.prototype.reset = function() {
  */
 var Selector = function() {
     this.sprite = 'images/selector.png';
-    this.x = iniXSelector;
-    this.y = iniYSelector;
+    this.x = INI_X_SELECTOR;
+    this.y = INI_Y_SELECTOR;
 };
 
 /**
@@ -223,22 +226,22 @@ Selector.prototype.render = function() {
 * @param {string} keyPressed, string that describes key pressed
 */
 Selector.prototype.handleInput = function(keyPressed) {
-    var dx = colWidth;
-    var dy = rowHeight;
+    var dx = COL_WIDTH;
+    var dy = ROW_HEIGHT;
     if (keyPressed == 'left') {
-        if (this.x - dx >= minXSelector) {
+        if (this.x - dx >= MIN_X_SELECTOR) {
             this.x -= dx;
         }
     } else if (keyPressed == 'right') {
-        if (this.x + dx <= maxXSelector) {
+        if (this.x + dx <= MAX_X_SELECTOR) {
             this.x += dx;
         }
     } else if (keyPressed == 'up') {
-        if (this.y - dy >= minYSelector) {
+        if (this.y - dy >= MIN_Y_SELECTOR) {
             this.y -= dy;
         }
     } else if (keyPressed == 'down') {
-        if (this.y + dy <= maxYSelector) {
+        if (this.y + dy <= MAX_Y_SELECTOR) {
             this.y += dy;
         }
     }
@@ -248,8 +251,8 @@ Selector.prototype.handleInput = function(keyPressed) {
 * @description Reset selector state.
 */
 Selector.prototype.reset = function() {
-    this.x = iniXSelector;
-    this.y = iniYSelector;
+    this.x = INI_X_SELECTOR;
+    this.y = INI_Y_SELECTOR;
 };
 
 /**********************************************************
@@ -263,8 +266,8 @@ Selector.prototype.reset = function() {
  */
 var Selectee = function() {
     this.sprite = 'images/star.png';
-    this.x = iniXSelector;
-    this.y = iniYSelector;
+    this.x = INI_X_SELECTOR;
+    this.y = INI_Y_SELECTOR;
 };
 
 /**
@@ -285,7 +288,7 @@ Selectee.prototype.handleInput = function(keyPressed) {
         if (this.x === selector.x && this.y === selector.y)
         {
             player.sprite = this.sprite;
-            gameMode = 'main-game';
+            gameMode = GAME_MODE_MAIN_GAME;
         }
     }
 };
@@ -338,16 +341,16 @@ Obstacle.prototype.update = function() {
 * using random number.
 */
 Obstacle.prototype.reset = function() {
-    var minC = minXObstacle/colWidth;
-    var minR = minYObstacle/rowHeight;
-    var maxC = maxXObstacle/colWidth;
-    var maxR = maxYObstacle/rowHeight;
+    var minC = MIN_X_OBSTACLE/COL_WIDTH;
+    var minR = MIN_Y_OBSTACLE/ROW_HEIGHT;
+    var maxC = MAX_X_OBSTACLE/COL_WIDTH;
+    var maxR = MAX_Y_OBSTACLE/ROW_HEIGHT;
 
     var rndC = Math.floor((Math.random() * (maxC - minC + 1)) + minC);
     var rndR = Math.floor((Math.random() * (maxR - minR + 1)) + minR);
 
-    this.x = rndC*colWidth;
-    this.y = rndR*rowHeight;
+    this.x = rndC*COL_WIDTH;
+    this.y = rndR*ROW_HEIGHT;
 };
 
 /**
@@ -407,16 +410,16 @@ Item.prototype.update = function() {
 * using random number.
 */
 Item.prototype.reset = function() {
-    var minC = minXItem/colWidth;
-    var minR = minYItem/rowHeight;
-    var maxC = maxXItem/colWidth;
-    var maxR = maxYItem/rowHeight;
+    var minC = MIN_X_ITEM/COL_WIDTH;
+    var minR = MIN_Y_ITEM/ROW_HEIGHT;
+    var maxC = MAX_X_ITEM/COL_WIDTH;
+    var maxR = MAX_Y_ITEM/ROW_HEIGHT;
 
     var rndC = Math.floor((Math.random() * (maxC - minC + 1)) + minC);
     var rndR = Math.floor((Math.random() * (maxR - minR + 1)) + minR);
 
-    this.x = rndC*colWidth;
-    this.y = rndR*rowHeight;
+    this.x = rndC*COL_WIDTH;
+    this.y = rndR*ROW_HEIGHT;
 
     this.isVisible = true;
 };
@@ -434,37 +437,33 @@ Item.prototype.render = function() {
  **********************************************************/
 
 // game initialization
-gameMode = 'player-selection';
-
-// game state
-// active - game is running
-gameState = 'active';
+gameMode = GAME_MODE_PLAYER_SELECTION;
 
 allSelectees = [];
-for (var i=0; i<numOfPlayerCharacters; i++) {
+for (var i=0; i<NUM_OF_PLAYABLE_CHARACTERS; i++) {
     allSelectees[i] = new Selectee();
-    allSelectees[i].x = i * colWidth;
-    allSelectees[i].sprite = playerSprites[i];
+    allSelectees[i].x = i * COL_WIDTH;
+    allSelectees[i].sprite = PLAYER_SPRITES[i];
 }
 
 selector = new Selector();
 
 allItems = [];
-for (var i=0; i<numOfItems; i++) {
+for (var i=0; i<NUM_OF_ITEMS; i++) {
     allItems[i] = new Item();
 }
 
 allEnemies = [];
-for (var i=0; i<numOfEnemies; i++) {
+for (var i=0; i<NUM_OF_ENEMIES; i++) {
     var row = (i % 3) + 1;
     var multiplier = (i % 5) + 1;
     allEnemies[i] = new Enemy();
-    allEnemies[i].y = minYEnemy + (row * rowHeight);
+    allEnemies[i].y = MIN_Y_ENEMY + (row * ROW_HEIGHT);
     allEnemies[i].speed *= multiplier;
 }
 
 allObstacles = [];
-for (var i=0; i<numOfObstacles; i++) {
+for (var i=0; i<NUM_OF_OBSTACLES; i++) {
     allObstacles[i] = new Obstacle();
 }
 
@@ -480,14 +479,14 @@ document.addEventListener('keyup', function(e) {
         80: 'p'
     };
 
-    if (gameMode == 'player-selection')
+    if (gameMode == GAME_MODE_PLAYER_SELECTION)
     {
         selector.handleInput(allowedKeys[e.keyCode]);
         allSelectees.forEach(function(selectee) {
             selectee.handleInput(allowedKeys[e.keyCode]);
         });
     }
-    else if (gameMode == 'main-game')
+    else if (gameMode == GAME_MODE_MAIN_GAME)
     {
         player.handleInput(allowedKeys[e.keyCode]);
     }
