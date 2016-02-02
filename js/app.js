1758daac2fc6c360f74d95438596d9c518ed017c
Tsuginosuke Sakauchi <tsuginosuke.sakauchi@fairlandcircle.com>
2016-02-02 07:46:53 -0600

    style: Remove unnecessary comments

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-// Constants
 var colWidth = 101;
 var rowHeight = 83;
 var numRows = 6;
@@ -59,23 +58,15 @@ playerSprites[4] = 'images/char-princess-girl.png';
  **********************************************************/
 // Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
     this.x = 0;
     this.y = 0;
     this.speed = 20;
 };
 
-// Update the enemy's position, required method for game
+// Update the enemy's position
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
     var dx = this.speed * dt;
     if (this.x + dx > maxXEnemy) {
         this.x = minXEnemy;
@@ -103,9 +94,8 @@ Enemy.prototype.update = function(dt) {
     isGameReset = true;
 };
 
-// Draw the enemy on the screen, required method for game
+// Draw the enemy on the screen
 Enemy.prototype.render = function() {
-    // ctx globally provided by engine.js
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
